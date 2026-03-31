7c6f87b30bad7bd51547b308d16415ff19827db4
WeirderMloon <frostbitefans@gmail.com>
2026-03-31 14:04:33 +1100

    f
    f

--- Assets/Scripts/SceneHandler.cs
+++ Assets/Scripts/SceneHandler.cs
@@ -4,15 +4,63 @@ public class SceneHandler : MonoBehaviour
 
 {
    
-    public void StartGame()
+    public void Menutolevelselect()
     {
 
         SceneManager.LoadScene(1);
     }
 
-    public void ChangeScene()
+    public void tolevelonestarter()
     {
 
         SceneManager.LoadScene(2);
+    }
+
+     public void ChangeLVL2()
+    {
+
+        SceneManager.LoadScene(6);
+    }
+
+    public void Changetolvl1()
+    {
+
+        SceneManager.LoadScene(5);
+    }
+
+    public void Lvltwostarter()
+    {
+
+        SceneManager.LoadScene(3);
+    }
+
+    public void Lvlthreestarter()
+    {
+
+        SceneManager.LoadScene(4);
+    }
+
+    public void Lvlthree()
+    {
+
+        SceneManager.LoadScene(7);
+    }
+
+    public void Lvl1toexit()
+    {
+
+        SceneManager.LoadScene(1);
+    }
+
+    public void Lvl2toexit()
+    {
+
+        SceneManager.LoadScene(1);
+    }
+
+    public void Lvlthreetoexit()
+    {
+
+        SceneManager.LoadScene(1);
     }
 }
